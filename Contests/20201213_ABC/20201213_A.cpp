74165fb5731f7dac9c91620b4b6c02068af608bf
rahasii <rahasii.2010@gmail.com>
2021-02-13 23:53:53 +0900

    AtCoder Beginner Contest 185 A

---
+++ Contests/20201213_ABC/20201213_A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 0, c = 0, d = 0;
+    cin >> a >> b >> c >> d;
+    cout << min({a, b, c, d}) << endl;
+    return 0;
+}
