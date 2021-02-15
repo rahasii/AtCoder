fadf706a95227b5f550be0608d98befd4ac4cba5
rahasii <rahasii.2010@gmail.com>
2021-02-15 20:00:01 +0900

    ABC185 A ABCの過去問をAtCoderProblemsに整理

---
+++ AtCoderProblems/ABC184/ABC184_A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    int A = 0, B = 0, C = 0, D = 0;
+    cin >> A >> B;
+    cin >> C >> D;
+    cout <<  A * D - B * C << "\n";
+    return 0;
+}
