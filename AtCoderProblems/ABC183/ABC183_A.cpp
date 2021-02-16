d39a8c5fa2b85843ab7472d8499354657258285d
rahasii <rahasii.2010@gmail.com>
2021-02-16 23:44:34 +0900

    ABC183 A

---
+++ AtCoderProblems/ABC183/ABC183_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x < 0)
+        x = 0;
+    cout << x << "\n";
+    return 0;
+}
