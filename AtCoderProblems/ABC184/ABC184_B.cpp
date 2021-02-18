049c9672140eb2511623580a5a304de5933624b6
rahasii <rahasii.2010@gmail.com>
2021-02-18 23:55:52 +0900

    ABC184 B

--- AtCoderProblems/ABC184/ABC184_B.cpp
+++ AtCoderProblems/ABC184/ABC184_B.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 //コンパイルの高速化
-#pragma GCC optimize("Ofast")
+// #pragma GCC optimize("Ofast")
 //endlの高速化
 #define endl "\n"
 //repマクロ
@@ -11,21 +11,9 @@ using P = pair<int, int>;
 
 int main()
 {
-    int N, X;
-    string s;
-    cin >> N >> X;
-    cin >> s;
-    rep(i, N)
-    {
-        if (s[i] == 'o')
-        {
-            ++X;
-        }
-        else
-        {
-            if (X > 0) --X;
-        }
-    }
-    cout << X << endl;
+    double a, b, c, d, katamuki, zero;
+    cin >> a >> b >> c >> d;
+    katamuki = (-d - b) / (c - a);
+    cout << printf("%.6f", (-b / katamuki) + a) << endl;
     return 0;
 }
